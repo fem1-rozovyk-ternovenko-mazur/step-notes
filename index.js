b191da0afdcf00581621c8ed59e5784d81d55f14
Iryna Rozovyk <iryna.rozovyk@gmail.com>
2019-08-01 18:55:35 +0300

    added previous functional, some styles edited

--- index.js
+++ index.js
@@ -17,15 +17,89 @@ client.connect(err => {
     app.db = collection;
 });
 
+
 app.use(express.static(__dirname + "/static"));
 
 app.set("view engine", "ejs");
 
+// Notes
 app.get("/", async (req, res)=>{
-    res.render("index")
+    let notes = []
+    await app.db.find({}).forEach((el) => {
+        notes.push(el)
+    });
 
+    res.render("index", {notes})
+
+})
+
+// Create note
+app.post("/create", async (req, res) => {
+    // Выводим данные из body в post
+    console.log(req.body)
+    try {
+        // Создаем в базе заметку
+        await app.db.insertOne({
+            ...req.body
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({created: true})
+})
+
+// Delete note
+app.post("/delete", async (req, res) => {
+    // Дані запиту
+    console.log(req.body.id)
+    try {
+        // Видалення нотатки по id
+        await app.db.deleteOne({
+            id: req.body.id
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({deleted: true})
+})
+
+//Edit note
+app.post("/edit", async (req, res) => {
+    // Виведення запиту з body в post
+    console.log(req.body)
+    try {
+        // Створення нотатки в базі
+        await app.db.updateOne({
+                id: req.body.id,
+
+            },
+            {
+                $set: {
+                    title: req.body.title,
+                    text: req.body.text
+                }
+            })
+    } catch (err) {
+        console.log(err)
+    }
+    res.json({edited: true})
+})
+
+// app.get("/", async (req, res)=>{
+//     res.render("index")
+//
+// })
+
+app.get('/:id', async (req, res) => {
+    let note;
+    await app.db.find({id: req.params.id }).forEach((el) => {
+        note = el
+    });
+
+    res.render("note", {note})
 })
 
 app.listen(port, ()=>{
-    console.log("hello in console")
+    console.log("kukusiki")
 })
+
