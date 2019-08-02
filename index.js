b958cdb95d48636ab938b76e41c250c3101f205f
Iryna Rozovyk <iryna.rozovyk@gmail.com>
2019-08-02 18:09:51 +0300

    some update

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
     console.log('BD connect error: ', err);
     const collection = client.db("STEP").collection("Notes");
+    // const collection = client.db("STEP").collection("ToDoList");
+
     app.db = collection;
+    // app.db2 = collection2;
+
 });
 
 app.use(express.static(__dirname + "/static"));
 
 app.set("view engine", "ejs");
 
-app.get("/", async (req, res)=>{
-    res.render("index")
-});
+// app.get("/", async (req, res)=>{
+//     res.render("index")
+// });
 
 app.get("/notes", async (req, res) => {
 
@@ -32,8 +36,28 @@ app.get("/notes", async (req, res) => {
 
 app.get("/lists", async (req, res) => {
 
-    res.render("create-list")
+    res.render("listcreate")
 });
+
+
+
+app.get("/", async (req, res)=>{
+    let notes = []
+    await app.db.find({}).forEach((el) => {
+        notes.push(el)
+    });
+    res.render("index", {notes})
+
+})
+//
+// app.get("/", async (req, res) => {
+//     let lists = []
+//     await app.db2.find({}).forEach((el) => {
+//         lists.push(el)
+//     });
+//     res.render("index", {lists})
+//     // res.render("listcreate")
+// });
 
 app.listen(port, ()=>{
     console.log("hello in console")
--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,207 @@
+// Кнопка Додати нотатку
+
 const addNoteBtn = document.querySelector("#addNoteButton");
-addNoteBtn.addEventListener("click", addNote);
 
-function addNote() {
-    window.location.href = '/notes'
-}
+addNoteBtn.addEventListener("click", () => {
+    window.location.href = '/notes';
+    let id = Date.now();
+    notesList.appendChild(getCardTemplate(id, "", "", true));
+    getCardBody(id).setAttribute("data-created", "false")
+});
 
-const addListBtn = document.querySelector("#addListButton");
-addListBtn.addEventListener("click", addList);
-
-function addList() {
-    window.location.href = '/lists'
-}
-
-
-
-//Елемент списка нотаток
+// Елемент списка нотаток
 let notesList = document.getElementById('notesList');
 // console.log(notesList);
 
-// Кнопка Додати список справ
-// let addList = document.getElementById('addListButton');
 
-//Елемент списка нотаток
+// Кнопка Додати список
+
+const addListBtn = document.querySelector("#addListButton");
+addListBtn.addEventListener("click", () => {
+    window.location.href = '/lists'
+});
+
+
+// Слушатель нажатий на кнопки
+notesList.addEventListener('click', function(e) {
+    // Обьявляем Id заметки
+    let id = e.target.dataset.id
+    // Идет разбор типов событий
+    if(e.target.classList.contains('btn-danger')) {
+        console.log('delete')
+        deleteNote(id)
+    } else if(e.target.classList.contains('save-btn')){
+        console.log('save')
+        if(getCardBody(id).dataset.edit){
+            editNote(id)
+        } else{
+            createNote(id)
+        }
+    } else if(e.target.classList.contains('edit-btn')){
+        console.log('edit')
+        let currentCol = getCol(id)
+        let newCol = getCardTemplate(id, getTitleVal(id, false), getTextVal(id, false), true)
+        currentCol.innerHTML = newCol.innerHTML
+        getCardBody(id).setAttribute("data-edit", "true");
+    } else if(e.target.classList.contains('card-body')){
+        if(e.target.dataset.created !== "false"){
+            window.location.href = `/${id}`
+        }
+    }
+})
+
+// Створення нотатки
+async function createNote(id){
+    let data = {
+        id: id,
+        title: getTitleVal(id, true),
+        text: getTextVal(id, true)
+    }
+    console.log(data)
+    let req = await fetch("http://localhost:3000/create-notes", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(data)
+    })
+
+    let answer = await req.json()
+    console.log(answer)
+    if(answer.created){
+        let currentCol = getCol(id)
+        let newCol = getCardTemplate(data.id, data.title, data.text, false)
+        currentCol.innerHTML = newCol.innerHTML
+    }
+
+}
+
+// Редагування нотатки
+async function editNote(id){
+    let data = {
+        id: id,
+        title: getTitleVal(id, true),
+        text: getTextVal(id, true)
+    }
+    console.log(data)
+    let req = await fetch("http://localhost:3000/edit", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(data)
+    })
+
+    let answer = await req.json()
+    console.log(answer)
+    if(answer.edited){
+        let currentCol = getCol(id)
+        let newCol = getCardTemplate(data.id, data.title, data.text, false)
+        currentCol.innerHTML = newCol.innerHTML
+    }
+}
+
+
+async function deleteNote(id) {
+    let data = {
+        id: id
+    }
+    let req = await fetch("http://localhost:3000/delete", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    let answer = await req.json()
+    console.log(answer)
+
+    if(answer.deleted){
+        let currentCol = getCol(id)
+        currentCol.remove()
+    }
+}
+
+
+function getCardTemplate( id, title, text, editStatus){
+
+    const inputElems = `
+        <div class="form-group">
+            <label for="note-title">Title</label>
+            <input type="text" class="form-control" id="note-title" value="${title}">
+        </div>
+        <div class="form-group">
+            <label for="note-text">Text</label>
+            <textarea class="form-control" id="note-text" rows="3" >${text}</textarea>
+        </div>`
+    const textElems = `
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${text}</p>`
+
+    let submitBtn,
+        neededContentElems
+
+    if(editStatus){
+        submitBtn = `
+        <button class="btn btn-primary save-btn" data-id="${id}">Save</button>
+        `
+        neededContentElems = inputElems
+    } else {
+        submitBtn = `
+        <button class="btn btn-success edit-btn" data-id="${id}">Edit</button>
+        `
+        neededContentElems = textElems
+    }
+
+
+    const cardContainer = `
+            <div class="card">
+                <div class="card-body" data-id="${id}">
+                    <div class="text-right">
+                        <button type="button" data-id="${id}" class="btn btn-danger">-</button>
+                    </div>
+                    ${neededContentElems}
+                    ${submitBtn}
+                </div>
+            </div>`
+
+    const wrapper = document.createElement('div')
+    wrapper.className = 'col-4'
+    wrapper.innerHTML = cardContainer
+    return wrapper
+}
+
+
+function getTitleVal(id, editStatus){
+    const tag = editStatus ? "input" : "h5"
+    const elem = document.querySelector(`.card-body[data-id="${id}"] ${tag}`)
+    if(editStatus){
+        return elem.value
+    } else{
+        return elem.innerText
+    }
+}
+
+function getTextVal(id, editStatus){
+    const tag = editStatus ? "textarea" : "p"
+    const elem = document.querySelector(`.card-body[data-id="${id}"] ${tag}`)
+    if(editStatus){
+        return elem.value
+    } else{
+        return elem.innerText
+    }
+}
+
+function getCol(id){
+    return document.querySelector(`.card-body[data-id="${id}"]`).parentNode.parentNode
+}
+
+function getCardBody(id){
+    return document.querySelector(`.card-body[data-id="${id}"]`)
+}
+
+
+
+// Елемент списка нотаток
 let checkList = document.getElementById('checkList');
 console.log(checkList);
