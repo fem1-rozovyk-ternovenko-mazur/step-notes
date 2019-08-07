8bbc166cac4786a34abf80b87fe7eb6ef4182401
Ternovenko_A <ternovenkoa@gmail.com>
2019-08-07 21:24:48 +0300

    Refactoring listcreate.finish work with line-through

--- static/js/listcreate.js
+++ static/js/listcreate.js
@@ -1,13 +1,13 @@
 const cancelNewList = document.querySelector('#cancelNewList');
 const saveNewList = document.querySelector('#saveNewList');
 const addNewItem = document.querySelector('#addNewItem');
-const writeListItem = document.querySelector('#writeListItem');
+//const writeListItem = document.querySelector('#writeListItem'); /*-- лишняя */
 const listArea = document.querySelector('#listArea');
 const todoList = [];
 
 
 cancelNewList.addEventListener('click', function(){
-//нужна ф-ция  очистить массив с заполненными пунктами, а так же очистить инпуты нзвания заметки и пункт
+//нужна ф-ция  очистить массив с заполненными пунктами, а так же очистить инпуты названия заметки и пункт
     //перейти на гавную стараницу
     window.location.href ="/";
 });
@@ -18,24 +18,68 @@ saveNewList.addEventListener('click', function(){
     window.location.href ="/";
 });
 
+
+
+
 addNewItem.addEventListener('click', function () {
     let valueListItem = document.querySelector('#writeListItem').value;
-    //создать ассоциативный массив с типа {todo: "...", check: false}
-    //где todo - список дела, а check - помечено ли дело как выполненное?  = false - дело не выполнено
-    let templateItem = {};
-    templateItem.todo = valueListItem;
-    templateItem.check = false;
-    let lengthTodoList = todoList.length;
-    todoList[lengthTodoList] = templateItem;
-    addItems();
-    //очистить поле ввода
-    writeListItem.value = null;
+    let div = document.createElement('div');
+    let checkbox = document.createElement("input");
+    let listText = document.createTextNode(valueListItem);
+    let label = document.createElement('label');
+    let spanRemove = document.createElement('span');
+    div.className = "list-group-item item-wrap"; /*- можно вносить строкой все нужные классы*/
+    label.className = 'label-wrap';
+    checkbox.type = 'checkbox';
+    checkbox.className = 'mr-3';
+    spanRemove.className = "remove";
+    spanRemove.innerText = "X";
+    label.appendChild(checkbox);
+    label.appendChild(listText);
+    div.appendChild(label);
+    div.appendChild(spanRemove);
+    if (valueListItem === ""){
+        alert("Порожній рядок не можна вносити!")
+    }else{
+        listArea.appendChild(div);
+    }
+    clearInputItem();
 });
 
-function addItems() {
-    let lineWithItems = "";
-    for (key in todoList){
-        lineWithItems += `<div class="list-group-item" data-item="item${Math.floor(100 + Math.random() * 900)}"><input type ="checkbox"><span class="ml-2">${todoList[key].todo}</span><span class="ml-5">X</span></div>`
-    }
-    listArea.innerHTML = lineWithItems;
+listArea.addEventListener('click', function (e) {
+    let target = e.target;
+     if (target.className === "remove"){
+         target.parentElement.remove();
+     }
+     if(target.className === "list-group-item item-wrap") {
+         console.log("YESS");
+         // if (target.checked) {
+         //     target.parentElement.className = "label-wrap crossed-text"
+         //
+         //
+         // } else {
+         //     target.parentElement.className ="label-wrap"
+         //
+         // }
+
+     }
+})
+
+       // lineWithItems += `<div class="list-group-item" data-item="item${Math.floor(100 + Math.random() * 900)}"><input type ="checkbox"><span class="ml-2">${todoList[key].todo}</span><span class="ml-5">X</span></div>`
+
+
+//как будет выглядеть объект созданной карточки
+/*
+{
+    id:"",
+    type: "list",
+    title: "",
+    body:[{text:"", checked: false/true}],
+}*/
+
+// добавить Модальное окошко как у Алины.
+
+
+function clearInputItem() {
+    document.querySelector('#writeListItem').value = null;
 }
