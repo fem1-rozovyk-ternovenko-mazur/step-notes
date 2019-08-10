249981d817a249d23eb5682efdeb3ee5d468ada3
Ternovenko_A <ternovenkoa@gmail.com>
2019-08-10 21:48:26 +0300

    finished deletion

--- static/js/lists.js
+++ static/js/lists.js
@@ -12,10 +12,38 @@ editList.addEventListener('click', function () {
 });
 
 deleteList.addEventListener('click', function () {
-
-    deleteThisList();
+    confirmNoteDeletion();
 });
 
+
+function confirmNoteDeletion(){
+
+    const confirmDeletionCard = document.createElement("div");
+    confirmDeletionCard.className = "confirm-exit-wrapper-list";
+    let body = document.querySelector('body');
+    let height = body.offsetHeight;
+    console.log(height);
+    confirmDeletionCard.style.height = `${height}px`;
+    confirmDeletionCard.innerHTML = `<div class="alert alert-info text-center text-dark">
+                                <span> Точно ВИДАЛИТИ нотатку? </span>
+                                <div class="row mt-3">
+                                    <div class="col">
+                                        <button class="btn btn-danger" id="confirmDeletionBtn"> Так, видали </button>
+                                    </div>
+                                    <div class="col">
+                                        <button class="btn btn-warning" id="cancelDeletionBtn"> Ні, хай живе </button>
+                                    </div>        
+                                </div>
+                            </div>`;
+
+    document.body.appendChild(confirmDeletionCard);
+
+    const confirmDeletionBtn = document.querySelector("#confirmDeletionBtn");
+    const cancelDeletionBtn = document.querySelector("#cancelDeletionBtn");
+
+    confirmDeletionBtn.addEventListener("click", deleteThisList);
+    cancelDeletionBtn.addEventListener("click", function (){document.body.removeChild(confirmDeletionCard)})
+}
 
 async function deleteThisList() {
     let data = {
