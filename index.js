711f06d358e52cfffe70ed2b758878c1ab5e2299
AlinaMgz <alinamgz@ukr.net>
2019-08-13 18:21:32 +0300

    heroku try 4

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.set("view engine", "ejs");
 
 // Getting documents from db to the main page
 
-app.get("https://fem1ns.herokuapp.com/fem1ns", async (req, res)=>{
-    let notes = []
+app.get("/", async (req, res)=>{
+    let notes = [];
     await app.db.find({}).forEach((el) => {
         notes.push(el)
     });
