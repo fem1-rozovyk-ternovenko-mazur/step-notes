0f0eef1378848485925d43a61a73c18a92f7fffe
Alina Mazur <54025456+Alinamgz@users.noreply.github.com>
2019-08-13 22:44:23 +0300

    Update create-note.js

--- static/js/create-note.js
+++ static/js/create-note.js
@@ -1,28 +1,28 @@
-
 const cancelBtn = document.querySelector("#cancelBtn");
 const saveNoteBtn = document.querySelector("#saveNoteBtn");
 
-cancelBtn.addEventListener("click", ifCancel);
-saveNoteBtn.addEventListener("click", saveNote );
-
-
-async function saveNote() {
-
-    let data = buildDataObject();
-
-    if (data.text !== "" || data.title !== "") { 
-        
+ cancelBtn.addEventListener("click", ifCancel);
+ 
+ saveNoteBtn.addEventListener("click", saveNote );
+ 
+ async function saveNote() { 
+ let data = buildDataObject();
+    if (data.text === "" || data.title === ""){ 
+        alert("А смисл зберігати пусту нотатку?")
+    };
+    
+    if(data.text !== "" && data.title !== ""){
     let req = await fetch('https://awesome-cat-notes.herokuapp.com/api/notes', {
         method: "POST",
-        headers: {
+        headers: { 
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(data),
-    });
-    let answer = await req.json();
-    if (answer.created){
+            body: JSON.stringify(data),
+     }); 
+    
+    let answer = await req.json()  
+  
+    if (answer.created){ 
         window.location.href = '/'
-    } else {
-        alert("А смисл зберігати пусту нотатку?")
     }
-}
+  };
